27149c64c3824ab94a8a978f4e2d0576b3d55e2c
Eljas Kinnunen <ekinnune@c1r1p7.hive.fi>
2023-02-03 16:38:07 +0200

    player class and some testing

--- Scripts/jintevaluator.cs
+++ Scripts/jintevaluator.cs
@@ -7,9 +7,15 @@ public class JintEvaluator
 {
     private Engine engine;
 
+
+
+    string log_return(string output)
+    {
+        return (output);
+    }
     public JintEvaluator()
     {
-        engine = new Engine().SetValue("log", new Action<object>(Debug.Log));
+        engine = new Engine().SetValue("log", new Action<object>(log_return));
     }
 
     public JsValue Evaluate(string code)
--- Scripts/missions.cs
+++ Scripts/missions.cs
@@ -11,8 +11,24 @@ public class Missions
         jint = new JintEvaluator();
     }
 
-    public void test(string code)
+    public string expected_out(Player player)
     {
-        Debug.Log(jint.Evaluate(code).ToString());
+        if (player.current_mission == 1)
+        {
+            return ("helloworld");
+        }
+        return ("");
+    }
+    public void test(string code, Player player)
+    {
+        string output = "";
+        jint.Evaluate(code).ToString();
+        string solution = expected_out(player).ToString();
+        if (output.Equals(solution))
+        {
+            Debug.Log("Got expected\n");
+        }
+        Debug.Log("expected " + expected_out(player));
+        Debug.Log("output " + output);
     }
 }
